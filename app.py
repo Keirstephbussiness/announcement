7fcc60b3ef99367527fa88fd419d763fcdf1520b
Keirstephbussiness <keirstephbussiness@gmail.com>
2025-09-11 15:21:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,12 @@ CORS(app)
 
 # Correct RSSHub route for NCST Facebook page
 RSSHUB_FEED_URL = "https://rsshub.app/facebook/page/NCST.OfficialPage"
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
 
 @app.route("/")
 def index():
